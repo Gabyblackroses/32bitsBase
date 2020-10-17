61c0fbe54f1024c00587bb545d3365d29011aa6e
Gabyblackroses <mgabypaz@hotmail.com>
2020-10-16 21:23:29 -0300

    v

---
+++ src/components/Search.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>{{totalStock}}</h2>
+    <list :games="availableGames"></list>
+    <h2>Busca tu juego</h2>
+    <input type="text" v-model="searchId">
+    <ul>
+        <li v-for="game in searchGamesById" :key="game.id">
+              {{game.id}}|{{game.name}}|{{game.stock}}|{{game.price}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script> 
+import {mapGetters} from "vuex"
+import List from "@/components/List" 
+export default {
+    data() {
+        return {searchId:""}
+    }, 
+    components: {
+        List
+    },
+    computed: {
+        ...mapGetters(["availableGames", "searchById", "totalStock"]), 
+        searchGamesById() {
+            return this.searchById(this.searchId)
+        }
+    
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
---
+++ src/store.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    title: "32Bits",
+    subtitle: "Juegos de PC y Consolas",
+    games: [{id: "0001", name: "Sekiro", stock: 100, price: 30000, color: "red", outstanding: true},
+      {id: "0002", name: "Fifa 21", stock: 100, price: 25000, color: "blue", outstanding: false},
+      {id: "0003", name: "Gears of War 4", stock: 100, price: 15000, color: "green", outstanding: true}, 
+      {id: "0004", name: "Mario Tennis Aces", stock: 100, price: 35000, color: "yellow", outstanding: false}, 
+      {id: "0005", name: "Bloodborne", stock: 100, price: 10000, color: "blue", outstanding: false},
+      {id: "0006", name: "Forza Horizon 4", stock: 100, price: 20000, color: "red", outstanding: true}]
+  },
+  getters: {
+    availableGames(state) {
+      return state.games.filter(game => game.stock > 0);
+    },
+    searchById:(_state, getters) => (id) => {
+      return getters.availableGames.filter(game => game.id == id)
+    },
+    totalStock(state) {
+      return state.games.reduce((acc,game)=> acc + game.stock, 0)
+    }
+  }, 
+  mutations: {},
+  actions: {}
+});
+
+export default store;
